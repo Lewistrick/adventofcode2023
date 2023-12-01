3fc668cf199afd43559e9331dfdd51d5dae625ab
e.wilts <e.wilts@ncim.nl>
2023-12-01 08:22:51 +0100

    Move part1 into part2 loop

--- o1.py
+++ o1.py
@@ -9,16 +9,6 @@ def find_all(string: str, substring: str) -> Generator[int, None, None]:
         from_idx = fi + 1
 
 
-tot = 0
-with open(r"1.in.txt") as f:
-    for line in f:
-        digits = [int(x) for x in line if x.isdigit()]
-        num = 10 * digits[0] + digits[-1]
-        tot += num
-print(tot)
-assert tot == 54159
-
-### PART 2
 vals = {
     t: i
     for i, t in enumerate(
@@ -26,20 +16,27 @@ vals = {
     )
 }
 
-
+tot1 = 0
 tot2 = 0
 with open(r"1.in.txt") as f:
     for line in f:
         digits = [(i, int(x)) for i, x in enumerate(line) if x.isdigit() and x != "0"]
+        # part 1
+        num1 = 10 * digits[0][1] + digits[-1][1]
+        tot1 += num1
+
+        # part 2
         words = []
         for val, j in vals.items():
             for fi in find_all(line, val):
                 digits.append((fi, j))
 
         digits = sorted(digits)
-
         num = digits[0][1] * 10 + digits[-1][1]
         tot2 += num
 
-print(tot2)
+assert tot1 == 54159
 assert tot2 == 53866
+
+print(tot1)
+print(tot2)
