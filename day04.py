bc6579f47d434bed46f2d56fd6ca2acb8824a45b
Lewistrick <erickwilts@gmail.com>
2023-12-04 06:52:00 +0100

    solve part 1, start part 2

---
+++ day04.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+
+def parse_line(line):
+    gameid, nums = line.split(": ")
+    left, right = nums.split(" | ")
+    win = set(map(int, left.split()))
+    own = set(map(int, right.split()))
+    noverlap = len(win & own)
+    return noverlap
+
+
+part1 = 0
+scores = {}  # game: score
+with open("04.example.in") as lines:
+    for i, line in enumerate(lines, 1):
+        score = parse_line(line)
+        if score:
+            part1 += 2 ** (score - 1)
+        scores[i] = score
+
+part2 = 0
+copies = defaultdict(int)  # game: n_copies (not counting original)
+for gameid in sorted(scores.keys()):
+    score = scores[gameid]
+    if not score:
+        continue
+    for i in range(gameid, gameid + score + 1):
+        copies[i] += 1
+    part2 += copies[gameid] + 1
+
+print(part1)
+print(part2)  # to do
