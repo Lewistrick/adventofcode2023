a12fe0fcdc3b1b64179707af536ad157930c3444
e.wilts <e.wilts@ncim.nl>
2023-12-04 09:21:50 +0100

    Cleanup

--- day04.py
+++ day04.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 
 def parse_line(line):
-    gameid, nums = line.split(": ")
+    _, nums = line.split(": ")
     left, right = nums.split(" | ")
     win = set(map(int, left.split()))
     own = set(map(int, right.split()))
@@ -22,18 +22,11 @@ with open("04.in") as lines:
 part2 = 0
 copies = defaultdict(int)  # game: n_copies (not counting original)
 for gameid in sorted(scores.keys()):
-    print("-" * 80)
-    print(f"{gameid=}")
     score = scores[gameid]
-    print(f"Score of this game: {score}")
     ncopies = copies[gameid] + 1
-    print(f"This game has {ncopies} copies (including the original)")
     for i in range(gameid + 1, gameid + score + 1):
-        print(f"Adding {ncopies} copies of game {i}")
         copies[i] += ncopies
-    print(sorted(copies.items()))
     part2 += ncopies
-    print(f"New total: {part2}")
 
 print(part1)
-print(part2)  # to do
+print(part2)
