0455410633da478d95446f6e18c31b1c5e82b0f1
e.wilts <e.wilts@ncim.nl>
2023-12-08 08:31:55 +0100

    More cleanup

--- day08.py
+++ day08.py
@@ -1,6 +1,8 @@
 from itertools import cycle
 from math import lcm
+from typing import Callable
 
+# parse input
 nodes: dict[str, tuple[str, str]] = {}
 
 with open("08.in") as f:
@@ -12,17 +14,12 @@ with open("08.in") as f:
         nodes[node] = (left, right)
 
 
-def get_cycle_length(nodes, instructions, currnode, end_determiner):
+def get_cycle_length(node: str, end_determiner: Callable[[str], bool]):
+    """Get the number of steps to get from currnode to the end."""
     for ii, instruction in enumerate(cycle(instructions), 1):
-        left, right = nodes[currnode]
-        match instruction:
-            case "L":
-                currnode = left
-            case "R":
-                currnode = right
-            case _:
-                raise ValueError("x")
-        if end_determiner(currnode):
+        left, right = nodes[node]
+        node = left if instruction == "L" else right
+        if end_determiner(node):
             return ii
 
 
@@ -30,17 +27,15 @@ curr_nodes = [node for node in nodes.keys() if node.endswith("A")]
 cycle_lengths = []
 for node in curr_nodes:
     node_cycle_length = get_cycle_length(
-        nodes,
-        instructions,
-        node,
-        lambda node: node.endswith("Z"),
+        node=node,
+        end_determiner=lambda node: node.endswith("Z"),
     )
     if node == "AAA":
         part1 = node_cycle_length
     cycle_lengths.append(node_cycle_length)
 
 # Two big assumptions here:
-# - every cycle only contains only one **Z
+# - every cycle only contains only one **Z node, which it ends with
 # - every cycle length is a multiple of the instructions length
 part2 = lcm(*cycle_lengths)
 
