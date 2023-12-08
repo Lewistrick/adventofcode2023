358ce6ba9806010a13eea957e88ed14b7d236b87
e.wilts <e.wilts@ncim.nl>
2023-12-08 08:27:20 +0100

    Remove duplicate code for 2 parts

--- day08.py
+++ day08.py
@@ -26,32 +26,23 @@ def get_cycle_length(nodes, instructions, currnode, end_determiner):
             return ii
 
 
-def end_determiner_p1(node: str):
-    return node == "ZZZ"
-
-
-currnode = "AAA"
-part1 = get_cycle_length(nodes, instructions, currnode, end_determiner_p1)
-print(part1)
-
-
-def end_determiner_p2(node: str):
-    return node.endswith("Z")
-
-
 curr_nodes = [node for node in nodes.keys() if node.endswith("A")]
 cycle_lengths = []
 for node in curr_nodes:
-    cycle_lengths.append(
-        get_cycle_length(
-            nodes,
-            instructions,
-            node,
-            end_determiner_p2,
-        )
+    node_cycle_length = get_cycle_length(
+        nodes,
+        instructions,
+        node,
+        lambda node: node.endswith("Z"),
     )
+    if node == "AAA":
+        part1 = node_cycle_length
+    cycle_lengths.append(node_cycle_length)
 
 # Two big assumptions here:
 # - every cycle only contains only one **Z
 # - every cycle length is a multiple of the instructions length
-print(lcm(*cycle_lengths))
+part2 = lcm(*cycle_lengths)
+
+print(part1)
+print(part2)
