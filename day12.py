30d8da78c4cd92646318d35444280bcc34482902
e.wilts <e.wilts@ncim.nl>
2023-12-12 09:25:30 +0100

    Remove unnecessary code/comments

--- day12.py
+++ day12.py
@@ -9,7 +9,7 @@ with open("12.in") as lines:
 
 
 @functools.lru_cache(maxsize=1000)
-def solve(record: str, counts: tuple[int], depth=1) -> int:
+def solve(record: str, counts: tuple[int]) -> int:
     if not counts:
         if all(ch in ".?" for ch in record):
             return 1
@@ -18,44 +18,26 @@ def solve(record: str, counts: tuple[int], depth=1) -> int:
     n_solutions = 0
     count, rest_counts = counts[0], counts[1:]
 
-    prefix = ">" * depth + " "
-    # print(f"{prefix}Solving for {record} {counts}")
-
-    # find all positions where `count` would fit
-    # print(f"{prefix}Checking starts from 0 to {len(record) - count}")
+    # Find all positions where `count` would fit
     for start in range(len(record) - count + 1):
         if any(ch == "#" for ch in record[:start]):
-            # print(f"{prefix}Skipped a #, not counting any more")
             break
         end = start + count
         subrecord = record[start:end]
-        # check if this spring fits
+
+        # Check if this spring fits
         if all(ch in "?#" for ch in subrecord):
             if end < len(record) and record[end] == "#":
-                # print(f"{prefix}{count} isn't bordered right at {start} : {end}")
                 continue
             if start > 0 and record[start - 1] == "#":
-                # print(f"{prefix}{count} isn't bordered left at {start} : {end}")
                 continue
 
-            # print(f"{prefix}{count} fits in {start} : {end}")
-            # if (count, subrecord_start, subrecord_end) == (1, 2, 3):
-            #     breakpoint()
             new_subrecord = record[end + 1 :]
-            n_sub_solutions = solve(new_subrecord, rest_counts, depth + 1)
-            # print(f"{prefix}Found {n_sub_solutions} intermediate solutions")
+            n_sub_solutions = solve(new_subrecord, rest_counts)
             n_solutions += n_sub_solutions
 
-    # print(f"{prefix}Found {n_solutions} solutions for {record} {counts}")
     return n_solutions
 
-
-assert solve("###", (3,)) == 1
-assert solve("???.###", (1, 1, 3)) == 1
-assert (sol := solve("?###????????", (3, 2, 1))) == 10, f"Wrong solution: {sol}"
-assert (sol := solve("#??.????#??.", (2, 1, 2, 1))) == 2, f"Wrong solution: {sol}"
-assert (sol := solve("??#???#?????.?", (5, 1, 1))) == 12, f"Wrong solution: {sol}"
-assert (sol := solve("???.??#????#?????", (3, 1))) == 3, f"Wrong solution: {sol}"
 
 part1 = 0
 for spring, counts in springs:
