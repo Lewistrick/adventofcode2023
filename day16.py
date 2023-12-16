2b8b4bdb92f3217c92eb80d53ab730d04c8f35c8
e.wilts <e.wilts@ncim.nl>
2023-12-16 07:26:18 +0100

    Cleanup; introduce helpers

--- day16.py
+++ day16.py
@@ -1,18 +1,12 @@
 from collections import defaultdict
 from functools import reduce
 
-mirrors = {}
-
-ddir = {  # dir : (dx, dy)
-    "R": (1, 0),
-    "L": (-1, 0),
-    "U": (0, -1),
-    "D": (0, 1),
-}
+from helpers import Pos, ddir
 
 with open("16.in") as f:
     lines = f.read().split()
 
+mirrors = {}
 empty = set()
 for y, line in enumerate(lines):
     for x, ch in enumerate(line.strip()):
@@ -20,9 +14,6 @@ for y, line in enumerate(lines):
             empty.add((x, y))
             continue
         mirrors[(x, y)] = ch
-
-
-Pos = tuple[int, int]
 
 
 def solve_by_start(
@@ -34,20 +25,16 @@ def solve_by_start(
     q = [(start, startd)]
     seen: dict[str, set[tuple[int, int]]] = defaultdict(set)
     while q:
-        # print(q)
         (x, y), d = q.pop()
-        # print(f"Currently at: {x, y} {d}")
 
         # Outside maze
         if (x, y) not in empty and (x, y) not in mirrors:
-            # print(f"Outside: {(x, y)}")
             continue
 
         # Mark as seen in this direction
         seen[d].add((x, y))
 
         if (x, y) in empty:
-            # print("Empty")
             dx, dy = ddir[d]
             newx, newy = x + dx, y + dy
 
@@ -57,9 +44,6 @@ def solve_by_start(
             q.append(((newx, newy), d))
             continue
 
-        # print(f"Encountered mirror: {mirrors[(x,y)]}")
-
-        #  | - \ /
         match mirrors[(x, y)], d:
             case "|", "L" | "R":
                 for nd in "UD":
@@ -102,12 +86,13 @@ def solve_by_start(
     return len(reduce(lambda s1, s2: s1 | s2, seen.values()))
 
 
+maxx = max(x for (x, y) in empty)
+maxy = max(y for (x, y) in empty)
+
 part1 = solve_by_start(mirrors, empty, (0, 0), "R")
 print(part1)
 
 part2 = 0
-maxx = max(x for (x, y) in empty)
-maxy = max(y for (x, y) in empty)
 for x in range(maxx):
     part2 = max(part2, solve_by_start(mirrors, empty, (x, 0), "D"))
     part2 = max(part2, solve_by_start(mirrors, empty, (x, maxy), "U"))
---
+++ helpers.py
@@ -0,0 +1,14 @@
+# typehints
+Pos = tuple[int, int]
+
+# directions (both relative (right/left/up/down) and absolute (east/west/north/south))
+ddir = {
+    "R": (1, 0),
+    "L": (-1, 0),
+    "U": (0, -1),
+    "D": (0, 1),
+    "E": (1, 0),
+    "W": (-1, 0),
+    "N": (0, -1),
+    "S": (0, 1),
+}
