d73043aee23dafceeae4d5791ae2b72a1362a230
e.wilts <e.wilts@ncim.nl>
2023-12-19 09:27:56 +0100

    Solve day 19, both parts (uncleaned)

--- day19.py
+++ day19.py
@@ -1,3 +1,4 @@
+import copy
 from dataclasses import dataclass
 
 
@@ -52,13 +53,85 @@ class Workflow:
         raise ValueError("No rule applied?")
 
 
+class WorkflowNode:
+    def __init__(self, name, x, m, a, s):
+        self.name = name
+        self.x: range = x
+        self.m: range = m
+        self.a: range = a
+        self.s: range = s
+        self.children: list["WorkflowNode"] = []
+
+    def create_children(self, workflows: dict[str, Workflow]):
+        if self.name in ("A", "R"):
+            return
+        fallback_ranges = {
+            "x": range(self.x.start, self.x.stop),
+            "m": range(self.m.start, self.m.stop),
+            "a": range(self.a.start, self.a.stop),
+            "s": range(self.s.start, self.s.stop),
+        }
+        # iterate over all workflows, except the last one (this is the fallback)
+        for check, comp, value, tgt in workflows[self.name].rules[:-1]:
+            child_ranges = copy.deepcopy(fallback_ranges)
+            # example: check=s comp=> value=2770 tgt=qs
+            if comp == ">":
+                # if s>2770 then go to qs
+                # let's say qs.s is [2000..3000]
+                # - then the new range should become [2771..3000]
+                # let's say fallback.s is [2000..3000]
+                # - then the new range should become [2000..2770]
+                child_ranges[check] = range(value + 1, child_ranges[check].stop)
+                fallback_ranges[check] = range(fallback_ranges[check].start, value)
+            # example: check=a comp=< value=2006 tgt=qkq
+            elif comp == "<":
+                # if a<2006 then go to qkq
+                # let's say qkq.a is [2000..3000]
+                # - then the new range should become [2000..2005]
+                # let's say fallback.a is [2000..3000]
+                # - then the new range should become [2006..3000]
+                child_ranges[check] = range(child_ranges[check].start, value - 1)
+                fallback_ranges[check] = range(value, fallback_ranges[check].stop)
+
+            child = WorkflowNode(name=tgt, **child_ranges)
+            child.create_children(workflows)
+            self.children.append(child)
+
+        fallback_wf_name: Workflow = workflows[self.name].rules[-1]
+        child = WorkflowNode(name=fallback_wf_name, **fallback_ranges)
+        child.create_children(workflows)
+        self.children.append(child)
+
+    def calculate_accepted(self):
+        if self.name == "R":
+            return 0
+        if self.name == "A":
+            total = 1
+            for ch in "xmas":
+                r: range = getattr(self, ch)
+                print(f"{ch}={r.stop-r.start+1}")
+                total *= r.stop - r.start + 1
+            print(f"Subtotal: {total}")
+            return total
+        return sum(child.calculate_accepted() for child in self.children)
+
+    def show(self, depth=0):
+        prefix = "--" * depth
+        print(f"{prefix} {self.name}")
+        for ch in "xmas":
+            r: range = getattr(self, ch)
+            print(f"{prefix} {ch} [{r.start}..{r.stop}]")
+        for child in self.children:
+            child.show(depth=depth + 1)
+
+
 with open("19.in") as f:
     rules_, parts_ = f.read().split("\n\n")
     rules = rules_.split("\n")
     parts = [Part.from_line(line) for line in parts_.split("\n")]
     wfs: list[Workflow] = (Workflow.from_line(line) for line in rules_.split("\n"))
 
-workflows = {wf.name: wf for wf in wfs}
+workflows: dict[str, Workflow] = {wf.name: wf for wf in wfs}
 
 A: list[Part] = []
 R: list[Part] = []
@@ -78,3 +151,16 @@ for part in parts:
 
 part1 = sum(p.rating for p in A)
 print(part1)
+
+wfn = WorkflowNode(
+    name="in",
+    x=range(1, 4000),  # should be inclusive ranges
+    m=range(1, 4000),
+    a=range(1, 4000),
+    s=range(1, 4000),
+)
+wfn.create_children(workflows)
+wfn.show()
+breakpoint()
+part2 = wfn.calculate_accepted()
+print(part2)
